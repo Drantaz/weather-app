7660c6c1482a482ed358f718884bcff1f0466ac8
jordannwabuike <jordannwabuike@gmail.com>
2023-11-06 16:25:21 +0000

    Done with week 2 activities

--- jordan-weather-app/src/pages/index.tsx
+++ jordan-weather-app/src/pages/index.tsx
@@ -1,12 +1,7 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main className={`flex min-h-screen flex-col items-center bg-red-500 justify-between p-24`}>
       <h1>Hello World!!!</h1>
     </main>
   )
