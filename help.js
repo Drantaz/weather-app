6f271087e115a8bec48b5204bde4a40c47aa2da7
Caleb Chibuike Okpara <93979386+Drantaz@users.noreply.github.com>
2023-11-16 11:02:28 +0000

    Update help.js

--- help.js
+++ help.js
@@ -10,3 +10,27 @@ for (let i = 0; i < lists.length; i++) {
     first5.push(obj);
   }
 }
+
+
+// Get current location
+
+if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+          },
+          (error) => {
+            Swal.fire(
+              "GPS Needed",
+              "Please enable your location in order to use this service.\nIf you have enabled it, please refresh the browser.",
+              "info"
+            );
+          }
+        );
+      } else {
+        Swal.fire(
+          "Error",
+          "Geolocation is not supported by this browser.",
+          "error"
+        );
+      } 
