c1c2eb5a840e6790576b59242fb7bc0b2364cb17
jordannwabuike <jordannwabuike@gmail.com>
2023-11-16 13:46:32 +0000

    Finally Done

--- jordan-weather-app/tailwind.config.ts
+++ jordan-weather-app/tailwind.config.ts
@@ -2,15 +2,9 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-<<<<<<< HEAD
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-=======
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
->>>>>>> 319ba063e00e8b8da8f5f09b177ee1b8ae2c6bf9
   ],
   theme: {
     extend: {
@@ -19,10 +13,7 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-<<<<<<< HEAD
       'dodgerblue': 'dodgerblue'
-=======
->>>>>>> 319ba063e00e8b8da8f5f09b177ee1b8ae2c6bf9
     },
   },
   plugins: [],
